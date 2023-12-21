dfcb1f8b76dd4978720a4c1bf408f07dd87399eb
Chloe <139293423+cbturner24@users.noreply.github.com>
2023-12-20 16:40:06 -0800

    -add stylesheet and js file

---
+++ script.js
@@ -0,0 +1,25 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n)
+{
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n)
+{let i;
+const slides = document.getElementsByClassName("mySlides");
+
+if (n> slides.length) {
+    slideIndex = 1;
+}
+if (n <1) {
+    slideIndex = slide.length;
+}
+for (i=0; i < slides.length; i++) {
+    slides[i].computedStyleMap.display = "none";
+}
+
+slides[slideIndex - 1].computedStyleMap.display = "block";
+
+}
