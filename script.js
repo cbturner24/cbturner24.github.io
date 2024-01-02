57de1ea9fcd84c724ecb094aeae4c1649fcd2bb1
Chloe <139293423+cbturner24@users.noreply.github.com>
2024-01-01 22:03:14 -0800

    add website links

--- script.js
+++ script.js
@@ -39,6 +39,14 @@ document.getElementById("closeAbout").addEventListener
 document.getElementById("aboutPopup").style.display = "none";
 });
 
+// add buttons for other sites 
+document.getElementById("Linkedin").addEventListener("click", function(){
+    window.open('www.linkedin.com/in/chloebturner', '_blank');
+});
+document.getElementById("Github").addEventListener("click", function(){
+    window.open('https://github.com/cbturner24/', '_blank');
+});
+
 //adding table of contents link event
 
 document.getElementById("LinkProject1").addEventListener("click", function () {
